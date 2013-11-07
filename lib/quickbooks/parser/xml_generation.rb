75189a866320997fea49e7a4c2ba10a37a5bebc9
Eggy <diego.link.eggy@gmail.com>
2013-11-07 15:54:42 -0800

    Fix stripping of false elements

--- lib/quickbooks/parser/xml_generation.rb
+++ lib/quickbooks/parser/xml_generation.rb
@@ -6,23 +6,23 @@ module Quickbooks::Parser::XMLGeneration
   def to_qbxml
     xml_doc = Nokogiri::XML(self.class.xml_template)
     root = xml_doc.root
-    log.debug "to_qbxml#nodes_size: #{root.children.size}"
+    #ap "to_qbxml#nodes_size: #{root.children.size}"
 
     # replace all children nodes of the template with populated data nodes
     xml_nodes = []
     root.children.each do |xml_template|
       next unless xml_template.is_a? XML_ELEMENT
       attr_name = underscore(xml_template)
-      log.debug "to_qbxml#attr_name: #{attr_name}"
+      #ap "to_qbxml#attr_name: #{attr_name}"
 
       val = self.send(attr_name)
-      next unless val && val.not_blank?
+      next if val != false && val.blank?
 
       xml_nodes += build_qbxml_nodes(xml_template, val)
-      log.debug "to_qbxml#val: #{val}"
+      #ap "to_qbxml#val: #{val}"
     end
 
-    log.debug "to_qbxml#xml_nodes_size: #{xml_nodes.size}"
+    #ap "to_qbxml#xml_nodes_size: #{xml_nodes.size}"
     root.children = xml_nodes.join('')
     set_xml_attributes!(root)
     root.to_s
